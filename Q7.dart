e5227f98088b6c205651678d15c46a8e44003d9f
Xtremiz <fozanahmed01@gmail.com>
2024-01-29 12:16:35 +0500

    Update Q7.dart

--- Q7.dart
+++ Q7.dart
@@ -4,8 +4,10 @@ void main() {
     'mon': 3000.0,
     'tue': 3234.0,
   };
-  print(expenses.containsKey("fri)")); // false because it does n't contain fri
-  expenses["fri"] = 5000.0; // adding fri to the map
-
-  print(expenses);
+  if (expenses.containsKey("fri")) {
+    print(expenses);
+  } else {
+    expenses["fri"] = 5000.0;
+    print(expenses);
+  }
 }
